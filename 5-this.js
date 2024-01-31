05af5bcb8f51784a936aea5d93de1ab0864df26f
yassinehaimouch <haymouch53@gmail.com>
2024-01-31 21:53:24 +0100

    this and set

--- 5-this.js
+++ 5-this.js
@@ -17,4 +17,8 @@ const greet = person.greet
 person.greet.call({name: "this is a bound object"})
 person.greet.apply({name: "this is a bound object"})
 
+//ES7 Arrow notation
+const newPerson = {name: 'newPerson', greet: () => {console.log(this.name)}}
+newPerson.greet() // undefined
+
 greet()
